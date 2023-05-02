0ac9545809649f693c576994e006306c6cbf37f3
mvalerko <04445326@mail.ru>
2023-05-02 13:02:05 +0200

    В конфигурации AppVeyor вернул Ubuntu. Попробовал добавить библиотекуWebDriverManager

--- src/test/java/ru/netology/web/TestWebForm.java
+++ src/test/java/ru/netology/web/TestWebForm.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.List;
 
